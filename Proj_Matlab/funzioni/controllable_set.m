b9b1583b69946ead71df8cf931f334f29bec1a00
DavideBrambilla98 <d.brambilla7@studenti.unibg.it>
2025-06-20 16:07:24 +0200

    Modifica visualizzazione CIS e Controllable_set
    - Usato Controllable_set visto nelle exe
    - Modificata la visualizzazione del CIS (ora vengono comparate le vasche 1-3 e 2-4)

--- Proj_Matlab/funzioni/controllable_set.m
+++ Proj_Matlab/funzioni/controllable_set.m
@@ -1,56 +1,29 @@
-% Calcola il controllable set in N passi verso un insieme target N>1 si blocca
-% function [H_nsteps, h_nsteps] = controllable_set(Hx, hx, Hu, hu, H_target, h_target, A, B, N)
+function [H_nsteps,h_nsteps] = controllable_set(Hx,hx,Hu,hu,H_target,h_target,A,B,N)
+%CONTROLLABLE_SET Calcolo dell'N-step-controllable set ad un set target
+%descritto da H_target * x <= h_target
 
-function [H_nsteps, h_nsteps, Np] = controllable_set(Hx, hx, Hu, hu, H_target, h_target, A, B, start)
-% Calcola l'N-step controllable set verso un insieme target.
-% Restituisce la rappresentazione H-rep del set e l'orizzonte minimo Np tale che lo stato iniziale è controllabile.
+n = size(A,2);
+m = size(B,2);
 
-    n = size(A, 2); % Dimensione dello stato
-    m = size(B, 2); % Dimensione dell'ingresso
+%   Candidato iniziale
+H_ii_steps = H_target;
+h_ii_steps = h_target;
 
-    % Parametri di configurazione
-    N_max = 50;             % Limite massimo di iterazioni
-    tol = 1e-6;             % Tolleranza numerica
+for ii=1:N
+    disp('iterazione')
+    disp(ii);
+    %   Computazione in R^(n+m)
+    temp = Polyhedron('A',[H_ii_steps*A H_ii_steps*B; ...
+        zeros(size(Hu,1),n) Hu],'b',[h_ii_steps; hu]);
+    %   Proiezione in R^n
+    temp = projection(temp,1:n);
+    temp.minHRep();
+    %   Intersezione con X := {x | Hx*x <= hx}
+    H_ii_steps = [temp.A; Hx];
+    h_ii_steps = [temp.b; hx];
+end
 
-    % Inizializzazione con il set target
-    H_ii_steps = H_target;
-    h_ii_steps = h_target;
-
-    Np = NaN; % Inizializza Np come non trovato
-
-    for ii = 1:N_max
-        % Costruzione del poliedro vincolato nel passo corrente
-        P = Polyhedron('A', H_ii_steps, 'b', h_ii_steps);
-
-        % Applica la dinamica inversa
-        temp = Polyhedron('A', [H_target*A, H_target*B; zeros(size(Hu,1),n), Hu], ...
-                          'b', [h_ii_steps; hu]);
-
-        % Proiezione sullo spazio degli stati x ∈ R^n
-        temp = projection(temp, 1:n);
-        temp = temp.minHRep(); 
-
-        % Interseca con X := {x | Hx*x <= hx}
-        H_ii_steps = [temp.A; Hx];
-        h_ii_steps = [temp.b; hx];
-
-        % Verifica se lo stato iniziale è contenuto nel set corrente
-        current_set = Polyhedron('A', H_ii_steps, 'b', h_ii_steps);
-        if current_set.contains(start, tol)
-            Np = ii;
-            break;
-        end
-    end
-
-    % Assegna comunque un valore alle uscite
-    if isnan(Np)
-        H_nsteps = [];
-        h_nsteps = [];
-    else
-        final_set = Polyhedron(H_ii_steps, h_ii_steps);
-        final_set = final_set.minHRep();
-        H_nsteps = final_set.A;
-        h_nsteps = final_set.b;
-    end
+H_nsteps = H_ii_steps;
+h_nsteps = h_ii_steps;
 
 end
--- Proj_Matlab/vincoloTermDisuguaglianza.m
+++ Proj_Matlab/vincoloTermDisuguaglianza.m
@@ -10,25 +10,25 @@ rng('default');
 %% Invariant set per il sistema controllato
 
 % Tempo di campionamento
-Ts = 60; %[s]
+Ts = 2; %[s]
 
 % Richiamo del modello del sistema dei serbatoio interconnessi
 addpath('funzioni');
 modello;
 
 % Matrici del costo quadratico
-Q = 1e1*eye(4); % Penalizza lo stato (quanto gli stati h1,h2,h3,h4 devono essere vicino al riferimento)
-R = 1e3*eye(2); % Penalizza l'ingresso (quanto limitare l'uso degli ingressi v1 e v2)
-
+Q = 20*eye(4); % Penalizza lo stato (quanto gli stati h1,h2,h3,h4 devono essere vicino al riferimento)
+R = 1*eye(2); % Penalizza l'ingresso (quanto limitare l'uso degli ingressi v1 e v2)
+Q = diag([10, 10, 1, 1]); % Penalizza lo stato (quanto gli stati h1,h2,h3,h4 devono essere vicino al riferimento)
+R = diag([0.1, 0.1]); % Penalizza l'ingresso (quanto limitare l'uso degli ingressi v1 e v2)
 % Control invariant set CIS_H*x <= CIS_h
 [CIS_H, CIS_h] = cis(sys_d.A, sys_d.B, zeros(4,1), zeros(2,1), Hx, hx, Hu, hu, Q, R); % si passano zeri come riferimento poichè il sistema è traslato sul riferimento
-CIS = Polyhedron(CIS_H, CIS_h);
-%
+
 %% Plot del CIS
 CIS_G = Polyhedron(CIS_H, CIS_h);
 CIS_G = CIS_G.minHRep();
-CIS_G_H12 = projection(CIS_G, 1:2);
-CIS_G_H34 = projection(CIS_G, 3:4);
+CIS_G_H13 = projection(CIS_G, [1,3]);
+CIS_G_H24 = projection(CIS_G, [2,4]);
 
 dim = CIS_G.Dim;
 disp(['La dimensione del poliedro è: ', num2str(dim)]);
@@ -37,40 +37,35 @@ disp(['La dimensione del poliedro è: ', num2str(dim)]);
 figure
 
 subplot(1 , 2 , 1)
-CIS_G_H12.plot();
-title("Proiezione del CIS dei livelli dei serbatoi 1 e 2")
-xlim([-20 20])
-ylim([-20 20])
+CIS_G_H13.plot();
+title("Proiezione del CIS dei livelli dei serbatoi 1 e 3")
 grid on
 axis equal
 xlabel("$h_1$" , Interpreter="latex")
-ylabel("$h_2$" , Interpreter="latex")
+ylabel("$h_3$" , Interpreter="latex")
 
 subplot(1 , 2 , 2)
-CIS_G_H34.plot();
-title("Proiezione del CIS dei livelli dei serbatoi 3 e 4")
-xlim([-20 20])
-ylim([-20 20])
+CIS_G_H24.plot();
+title("Proiezione del CIS dei livelli dei serbatoi 2 e 4")
 grid on
 axis equal
-xlabel("$h_3$" , Interpreter="latex")
+xlabel("$h_2$" , Interpreter="latex")
 ylabel("$h_4$" , Interpreter="latex")
 
 %% N-step controllable set
 
 % Orizzonte di predizione fissato
-%N = 1;
-%fprintf('\n--- Calcolo del %d-step controllable set ---\n', N);
-%[Np_steps_H, Np_steps_h] = controllable_set(Hx, hx, Hu, hu, CIS_H, CIS_h, sys_d.A, sys_d.B, N);
-%fprintf('Vincoli nel %d-step set: %d\n', N, size(Np_steps_H,1));
+N = 1;
+fprintf('\n--- Calcolo del %d-step controllable set ---\n', N);
+[Np_steps_H, Np_steps_h] = controllable_set(Hx, hx, Hu, hu, CIS_H, CIS_h, sys_d.A, sys_d.B, N);
+fprintf('Vincoli nel %d-step set: %d\n', N, size(Np_steps_H,1));
 
-[Np_steps_H, Np_steps_h , Np] = controllable_set(Hx, hx, Hu, hu, CIS_H, CIS_h, sys_d.A, sys_d.B, x0_centrato);
+% [Np_steps_H, Np_steps_h , Np] = controllable_set(Hx, hx, Hu, hu, CIS_H, CIS_h, sys_d.A, sys_d.B, x0_centrato);
 
 % Costruzione poliedro
 Np_steps_set = Polyhedron(Np_steps_H, Np_steps_h);
 Np_steps_set = Np_steps_set.minHRep();
 
 %% plot
-
 
 %% MPC e simulazione
